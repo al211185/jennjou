e9ce705d4053e5ef4baee759e1ed61439f5253c8
al211185 <al211185@alumnos.uacj.mx>
2025-11-18 16:44:26 -0700

    ajustes de color

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const repoName = "jennjou";
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
-    unoptimized: true,
+    unoptimized: true, // si quieres, puedes quitar esto y usar la optimización de Vercel
     remotePatterns: [
       {
         protocol: "https",
