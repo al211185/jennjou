772774e18b9030dca878e11d891692c10ed7dbd8
jennjou <92053617+al211185@users.noreply.github.com>
2025-11-25 19:49:17 -0700

    Optimize media loading

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // si quieres, puedes quitar esto y usar la optimización de Vercel
     remotePatterns: [
       {
         protocol: "https",
